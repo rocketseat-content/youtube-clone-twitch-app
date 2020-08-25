b7a723dc9a3c2b9a33e4bb464cf8fcca069ceb27
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2020-08-25 17:22:55 -0300

    Finished 404 page

--- src/pages/ComingSoon/index.tsx
+++ src/pages/ComingSoon/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Text } from 'react-native';
 
-import { Container } from './styles';
+import { Wrapper, Heading } from './styles';
 
 const ComingSoon: React.FC = () => {
   return (
-    <Container>
-      <Text>ComingSoon</Text>
-    </Container>
+    <Wrapper>
+      <Heading>Coming soon...</Heading>
+    </Wrapper>
   );
 };
 
--- src/pages/ComingSoon/styles.ts
+++ src/pages/ComingSoon/styles.ts
@@ -1,5 +1,23 @@
 import styled from 'styled-components/native';
+import Constants from 'expo-constants';
+import { Platform } from 'react-native';
 
-export const Container = styled.View`
+import colors from '../../styles/colors';
 
+const statusBarHeight =
+  Platform.OS === 'android' ? Constants.statusBarHeight : 0;
+
+export const Wrapper = styled.SafeAreaView`
+  background: ${colors.primary};
+  flex: 1;
+  padding-top: ${statusBarHeight + 'px'};
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Heading = styled.Text`
+  color: ${colors.purple};
+  font-family: roboto_500;
+  font-size: 30px;
 `;
