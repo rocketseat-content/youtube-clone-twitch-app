a8dd97fd3d6679fd652f7c29074218146f72ff2f
Guilherme Rodz <guilherme.rodrigues.profissional@gmail.com>
2020-08-25 17:29:37 -0300

    Setup bottom tab icons

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+
+import colors from './styles/colors';
 
 import Following from './pages/Following';
 import ComingSoon from './pages/ComingSoon';
@@ -10,10 +13,66 @@ const { Navigator, Screen } = createBottomTabNavigator();
 const Routes: React.FC = () => (
   <NavigationContainer>
     <Navigator>
-      <Screen name="Following" component={Following} />
-      <Screen name="Discover" component={ComingSoon} />
-      <Screen name="Browse" component={ComingSoon} />
-      <Screen name="Esports" component={ComingSoon} />
+      <Screen
+        name="Following"
+        component={Following}
+        options={{
+          tabBarIcon: ({ size, focused }) => {
+            return (
+              <Ionicons
+                name="md-heart"
+                size={size}
+                color={focused ? colors.purple : colors.black}
+              />
+            );
+          },
+        }}
+      />
+      <Screen
+        name="Discover"
+        component={ComingSoon}
+        options={{
+          tabBarIcon: ({ size, focused }) => {
+            return (
+              <MaterialCommunityIcons
+                name="compass-outline"
+                size={size}
+                color={focused ? colors.purple : colors.black}
+              />
+            );
+          },
+        }}
+      />
+      <Screen
+        name="Browse"
+        component={ComingSoon}
+        options={{
+          tabBarIcon: ({ size, focused }) => {
+            return (
+              <Ionicons
+                name="md-browsers"
+                size={size}
+                color={focused ? colors.purple : colors.black}
+              />
+            );
+          },
+        }}
+      />
+      <Screen
+        name="Esports"
+        component={ComingSoon}
+        options={{
+          tabBarIcon: ({ size, focused }) => {
+            return (
+              <MaterialCommunityIcons
+                name="trophy-outline"
+                size={size}
+                color={focused ? colors.purple : colors.black}
+              />
+            );
+          },
+        }}
+      />
     </Navigator>
   </NavigationContainer>
 );
